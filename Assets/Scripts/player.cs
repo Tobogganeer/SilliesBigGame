91685180aaa90b0cd1f288299d7e7ab2518d80a2
ToastKing1 <owenspreadbury1@gmail.com>
2025-01-27 17:44:36 -0500

    item data script and item combination script made, open/close inventory button created

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
 
     public GameObject Camera;
+    public GameObject InventoryPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,11 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    public void InventoryToggle()
+    {
+        InventoryPanel.SetActive(!InventoryPanel.activeInHierarchy);
     }
 }
