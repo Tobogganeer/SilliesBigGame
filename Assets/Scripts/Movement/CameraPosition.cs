5445f0ed720141c124447181962509ba7de2c8d2
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-02-03 17:16:13 -0500

    Drawing gizmos to visualize transitions

--- Assets/Scripts/Movement/CameraPosition.cs
+++ Assets/Scripts/Movement/CameraPosition.cs
@@ -51,6 +51,44 @@ public class CameraPosition : MonoBehaviour
         Down,
         Custom
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.2f);
+
+        const float Offset = 1f;
+
+        foreach (CameraRotation state in rotations)
+        {
+            // Draw direction itself
+            Gizmos.color = state.facing.GetColour();
+            Gizmos.DrawSphere(GetPosition(state.facing), 0.1f);
+
+            // Draw connections
+            //Gizmos.color = Color.white;
+            foreach (Transition trans in state.transitions)
+            {
+                // Another rotation on this object
+                if (trans.mode == Transition.Mode.AnotherRotation)
+                {
+                    Gizmos.DrawLine(GetPosition(state.facing), GetPosition(trans.leadsToRotation));
+                    Gizmos.DrawWireSphere(GetPosition(trans.leadsToRotation), 0.13f);
+                }
+                // Check if the other object has been assigned
+                else if (trans.leadsToPosition != null)
+                {
+                    Vector3 targetPosition = trans.leadsToPosition.transform.position;
+                    Vector3 targetOffset = trans.leadsToRotation.GetOffset() * Offset;
+
+                    Gizmos.DrawLine(GetPosition(state.facing), targetPosition + targetOffset);
+                    Gizmos.DrawWireSphere(targetPosition + targetOffset, 0.13f);
+                }
+            }
+        }
+
+        Vector3 GetPosition(CameraDirection dir) => transform.position + dir.GetOffset() * Offset;
+    }
 }
 
 /// <summary>
@@ -82,4 +120,29 @@ public enum CameraDirection
     /// Y-
     /// </summary>
     Down
+}
+
+static class CameraDirectionExtensions
+{
+    public static Vector3 GetOffset(this CameraDirection direction) => direction switch
+    {
+        CameraDirection.North => Vector3.forward,
+        CameraDirection.East => Vector3.right,
+        CameraDirection.South => Vector3.back,
+        CameraDirection.West => Vector3.left,
+        CameraDirection.Up => Vector3.up,
+        CameraDirection.Down => Vector3.down,
+        _ => throw new NotImplementedException(),
+    };
+
+    public static Color GetColour(this CameraDirection direction) => direction switch
+    {
+        CameraDirection.North => new Color(0.3f, 0.3f, 0.7f),
+        CameraDirection.East => new Color(0.7f, 0.3f, 0.3f),
+        CameraDirection.South => new Color(0.2f, 0.2f, 0.8f),
+        CameraDirection.West => new Color(0.8f, 0.2f, 0.2f),
+        CameraDirection.Up => new Color(0.3f, 0.7f, 0.3f),
+        CameraDirection.Down => new Color(0.2f, 0.8f, 0.2f),
+        _ => throw new NotImplementedException(),
+    };
 }
