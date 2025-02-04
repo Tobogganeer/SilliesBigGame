a79c18906316873940e3ec8577ad143f458653a9
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-02-04 17:23:31 -0500

    Example code

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Tobo.Util;
 using UnityEngine;
 
 public class ItemData : MonoBehaviour
 {
-    public Dictionary<string, Dictionary<string, string>> itemData = new Dictionary<string, Dictionary<string, string>>();
+    public SerializableDictionary<string, ItemSO> items;
 
+    public Dictionary<string, Dictionary<string, string>> itemData = new Dictionary<string, Dictionary<string, string>>();
 
     private void Start()
     {
+
         itemData.Add("Flashlight", new Dictionary<string, string> { 
             { "itemDescription", "To venture in the dark" },
             {"itemImage", "ItemSprites/flashlightTest"}
