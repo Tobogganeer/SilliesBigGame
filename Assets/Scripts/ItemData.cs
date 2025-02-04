bca4f02aa75423d2b782719c11e0f9a3564ff36d
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-02-04 17:27:24 -0500

    Singleton example

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class ItemData : MonoBehaviour
 {
+    public static ItemData instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
     public SerializableDictionary<string, ItemSO> items;
 
     public Dictionary<string, Dictionary<string, string>> itemData = new Dictionary<string, Dictionary<string, string>>();
 
     private void Start()
     {
-
         itemData.Add("Flashlight", new Dictionary<string, string> { 
             { "itemDescription", "To venture in the dark" },
             {"itemImage", "ItemSprites/flashlightTest"}
@@ -25,6 +30,12 @@ public class ItemData : MonoBehaviour
             {"itemImage", "ItemSprites/flashlightTest"}
         });
 
+    }
+
+    // Usage (from anywhere): 'ItemData.GetItem("Battery").icon' etc
+    public static ItemSO GetItem(string id)
+    {
+        return instance.items[id];
     }
 
 }
