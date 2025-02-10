f69dcb85c4bc07911e70afc53c0967e2b902a8f5
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-02-09 21:19:24 -0500

    Updated billboard to lock to be vertical

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Tobo.Util;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
     public Mode mode;
-    public Vector3 rotationOffset;
+    //public Vector3 rotationOffset;
+    public bool lockToVerticalAxis = true;
     Transform cam;
 
     private void Start()
@@ -15,11 +17,16 @@ public class Billboard : MonoBehaviour
 
     private void Update()
     {
-        if (mode == Mode.FaceCamera)
-            transform.LookAt(cam);
-        else
-            transform.forward = -cam.forward;
-        transform.Rotate(rotationOffset, Space.Self);
+        Vector3 lookDir = transform.position.Dir(cam.position);
+        if (mode == Mode.MatchCameraForwardVector)
+            lookDir = -cam.forward;
+
+        if (lockToVerticalAxis)
+            lookDir.y = 0f;
+
+        transform.forward = lookDir.normalized;
+
+        //transform.Rotate(rotationOffset, Space.Self);
     }
 
     public enum Mode
