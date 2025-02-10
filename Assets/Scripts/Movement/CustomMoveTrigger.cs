c264022f2d65a4f69f24ec485dfd1079420ca5f4
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-02-09 23:37:02 -0500

    Disabled all custom move triggers on start
    Wasn't finding ones with disabled objects before

--- Assets/Scripts/Movement/CustomMoveTrigger.cs
+++ Assets/Scripts/Movement/CustomMoveTrigger.cs
@@ -3,17 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class CustomMoveTrigger : MonoBehaviour, IInteractable
 {
     public event Action OnClicked;
 
     Collider coll;
-    private void Awake()
-    {
-        SetColliderEnabled(false);
-    }
-
     Collider GetCollider()
     {
         if (coll == null)
