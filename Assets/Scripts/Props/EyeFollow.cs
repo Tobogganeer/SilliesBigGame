4c7cba1eb4246cd674c9ba8d67ea2c2525b2ed6d
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-02-16 11:32:37 -0500

    Changed patient eye code

--- Assets/Scripts/Props/EyeFollow.cs
+++ Assets/Scripts/Props/EyeFollow.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class EyeFollow : MonoBehaviour
 {
-    public float amount = 0.00001f;
-    public Vector3 mask = new Vector3(1, 1, 0);
-    public float delay = 0.2f;
+    public float multiplier = 0.01f;
+    public float maxAmount = 0.01f;
 
-    Vector3 startingPosition;
-    public Vector3 position;
+    Vector3 startingLocalPosition;
+    Transform parent;
+    Camera cam;
 
-    private IEnumerator Start()
+    private void Start()
     {
-        startingPosition = transform.localPosition;
-        WaitForSeconds wait = new WaitForSeconds(delay);
+        startingLocalPosition = transform.localPosition;
+        cam = Camera.main;
+        parent = transform.parent;
+    }
 
-        while (true)
-        {
-            position = Mouse.current.position.ReadValue() * amount;
+    Vector3 GetStartingPosition() => parent.position + startingLocalPosition;
 
-            transform.localPosition = startingPosition + Vector3.Scale(position, mask);
-            yield return wait;
-        }
+    private void Update()
+    {
+        Vector3 cursorScreenPos = Mouse.current.position.value;
+        // Set z so the world position is accurate
+        cursorScreenPos.z = Vector3.Distance(GetStartingPosition(), cam.transform.position);
+
+        Vector3 cursorPos = cam.ScreenToWorldPoint(cursorScreenPos);
+        Vector3 offset = cursorPos - GetStartingPosition();
+
+        offset = Vector3.ClampMagnitude(offset * multiplier, maxAmount);
+
+        transform.position = GetStartingPosition() + offset;
     }
 }
