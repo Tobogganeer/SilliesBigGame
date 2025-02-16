7aaf79aa400d9f0732918db85f7a456d53b56d61
Simran <simranbaria06@gmail.com>
2025-02-15 23:01:47 -0500

    Added and updated art
    Updated Player prop because there was a mistake that literally no one else but me would notice
    Added CGs for computer and Tutorial QTE
    CG interaction functional

--- Assets/Scripts/Props/Patient.cs
+++ Assets/Scripts/Props/Patient.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Patient : MonoBehaviour, IInteractable, ICustomCursor
 {
+    public GameObject CG;
+
     public void OnClicked()
     {
-        if (Inventory.HasItem("Syringe"))
-        {
-            PopUp.Show("End of milestone gameplay");
-        }
+        if (Inventory.HasItem("Syringe")) CG.SetActive(enabled);
     }
 
     public CursorType GetCursorType()
