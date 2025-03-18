b942b0acba4ef74287e0c5aff3065ef8dcef39f5
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-03-18 12:54:19 -0400

    Added telemetry session display to screen

--- Assets/Scripts/Telemetry/SessionDisplay.cs
+++ Assets/Scripts/Telemetry/SessionDisplay.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class SessionDisplay : MonoBehaviour
 {
+    public TMPro.TMP_Text sessionIDText;
+
     public void OnConnected(int sessionID)
     {
         PopUp.Show("Session #" + sessionID);
+        sessionIDText.text = "Session #" + sessionID;
     }
 
     public void OnConnectionFailed(string reason)
     {
         PopUp.Show("Telemetry connection failed: " + reason);
+        sessionIDText.text = "Telemetry connection failed: " + reason;
     }
 }
