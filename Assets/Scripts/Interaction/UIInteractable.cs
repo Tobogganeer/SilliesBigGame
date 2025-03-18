2579865b42ec7e714860a87caa05bde8bc859a67
Tobogganeer <66337562+Tobogganeer@users.noreply.github.com>
2025-03-18 16:30:13 -0400

    Made CG colliders scale properly

--- Assets/Scripts/Interaction/UIInteractable.cs
+++ Assets/Scripts/Interaction/UIInteractable.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 /// <summary>
 /// Makes a UI object interact with the Interactable system
 /// </summary>
 public class UIInteractable : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler
 {
+    public bool raycastHitsTransparentPixels = true;
+
+    private void Start()
+    {
+        // If we don't want to hit transparent pixels, set the minimum alpha to 0.5
+        if (!raycastHitsTransparentPixels)
+            GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+
     public static UIInteractable CurrentlyHovered;
 
     public void OnPointerEnter(PointerEventData eventData)
