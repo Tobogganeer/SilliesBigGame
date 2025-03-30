dbb5bd412efc3dbc17c54ede0c4f8deccb213bca
Simran <simranbaria06@gmail.com>
2025-03-30 02:06:34 -0400

    Importing assets and shit

---
+++ Assets/Scripts/Props/SurgeryID.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SurgeryID : MonoBehaviour
+{
+    public TMP_Dropdown procedure, equipment, medication, duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateID()
+    {
+        
+    }
+}
