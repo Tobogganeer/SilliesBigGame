944216297d211945b22e4007ec1264ceee769fc6
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2025-04-07 21:31:28 -0400

    Made pause menu not exit game

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -30,10 +31,13 @@ public class PauseMenu : MonoBehaviour
 
     public void ExitToMainMenu()
     {
+        SceneManager.LoadScene("TitleScreen");
+        /*
         // TODO: Exit to main menu once implemented
         Application.Quit();
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+        */
     }
 }
